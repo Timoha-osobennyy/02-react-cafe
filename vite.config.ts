3bee6906e7822ceb22beda9ee06b8daaaf645203
Timoha-osobennyy <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-07-18 01:56:53 +0300

    reackte777

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/02-react-cafe/', 
-  plugins: [react()],
-  server: {
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    chunkSizeWarningLimit: 1600
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const base = env.VERCEL ? '/' : '/02-react-cafe/'
+
+  return {
+    base,
+    plugins: [react()],
+    server: {
+      open: true,
+      port: 3000
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name]-[hash][extname]',
+          chunkFileNames: 'assets/[name]-[hash].js',
+          entryFileNames: 'assets/[name]-[hash].js'
+        }
+      }
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCase'
+      }
     }
   }
 })
