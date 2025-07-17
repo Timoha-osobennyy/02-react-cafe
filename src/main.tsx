ba93c42c5ce8cac9e8458f0830c2f10ec31d622c
Timoha-osobennyy <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-07-18 00:11:39 +0300

    GrandWork

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import "modern-normalize/modern-normalize.css";
+import "modern-normalize"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
